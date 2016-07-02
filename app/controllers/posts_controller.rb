31b5b1fab9ff6d0db79fba31eed4c7dbd06a8ba4
eaylward8 <eaylward8@gmail.com>
2016-07-02 13:00:50 -0400

    Work on PostsController CRUD actions and views - new, create, show, index - add simple_forms gem - add new and show views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,24 @@
 class PostsController < ApplicationController
 
   def index
-    
+    @posts = Post.all
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -28,4 +33,9 @@ class PostsController < ApplicationController
 
   end
 
+  private
+
+    def post_params
+      params.require(:post).permit(:image, :caption)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :image, presence: true
 
   # re-size image to Instagram default width
-  has_attached_file :image, styles: {medium: 640x}
+  has_attached_file :image, styles: { medium: '640x' }
   # validate image format
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
