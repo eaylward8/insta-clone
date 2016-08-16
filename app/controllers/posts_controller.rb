95bd1d647e7b6e1a55276e08442ae4fa9f29b147
eaylward8 <eaylward8@gmail.com>
2016-08-16 15:57:27 -0400

    Finish edit, update, destroy actions; Create form partial for new/edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,11 +26,19 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
 
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
